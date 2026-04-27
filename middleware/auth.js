cad5a22726ea5d49152e0fdad6f20a9990e1ec48
prygw <pry2037@gmail.com>
2026-04-26 21:23:32 -0700

    add jwt signing middleware, install jsonwebtoken

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+//takes user id (string) as input and returns a signed jwt 
+function genToken(userid) {
+	//could add expiry later
+	return jwt.sign({id: userid}, process.env.JWT_SECRET);
+}
+function checkAuth(req, res, next) {
+	//if a path requires auth, the api reqs must contain an auth header with a signed jwt
+	const header = req.headers.auth;
+	if (!header || !header.startsWith("Bearer ")) {
+		return res.status(401).json({error: "Malformed Auth or Does not Exist"});
+	}
+	//if it does exist, and is in proper format: "Bearer [jwt]", we process it
+	try {
+		const token = header.split(" ")[1];
+		req.userId = jwt.verify(token, process.env.JWT_TOKEN);
+		next();
+	}
+	catch(err)
+	{
+		return res.status(401).json({error: "Malformed Auth"});
+	}
+}
+
+module.exports = {genToken, checkAuth};
