a4983b1a94d92be1e05919990d8e9e75bf3532c1
prygw <pry2037@gmail.com>
2026-05-16 01:01:05 -0700

    made requireAdmin middleware work

--- server/middleware/requireAdmin.js
+++ server/middleware/requireAdmin.js
@@ -1,7 +1,22 @@
-const requireAdmin = (req, res, next) => {
-    // build actual admin verification later.
-    console.log("Everyone is admin right now...");
-    next();
-};
+const { getDb } = require("../db");
+
+async function requireAdmin(req, res, next)
+{
+    try {
+        if (req.userId)
+        {
+            const db = await getDb();
+            const user = await db.get("SELECT is_admin from users WHERE id=?", [req.userId]);
+            if (!user || !user.is_admin)
+            {
+                return res.status(403).json({error: "Admin Access required."})
+            }
+            next();
+        }
+    }
+    catch(err) {
+        return res.status(500).json({error: "Not admin."});
+    }
+}
 
 module.exports = { requireAdmin };
