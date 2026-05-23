e7c1fe92fca40f8367ff1ca62423fa91e0906a90
prygw <pry2037@gmail.com>
2026-05-22 21:07:22 -0700

    add backend for leaderboard and portfolio

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,7 @@ router.use('/markets', require('./markets'));
 router.use('/bets', require('./bets'));
 router.use('/', require('./health'));
 router.use('/auth', require('./auth'));
+router.use('/portfolio', require('./portfolio'));
+router.use('/leaderboard', require('./leaderboard'));
 
 module.exports = router;
