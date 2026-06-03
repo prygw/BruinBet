2a028adf3136285c8452e333e2a04f3fa188d66c
prygw <pry2037@gmail.com>
2026-06-03 13:44:54 -0700

    add comment POST and GET

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,5 +8,5 @@ router.use('/', require('./health'));
 router.use('/auth', require('./auth'));
 router.use('/portfolio', require('./portfolio'));
 router.use('/leaderboard', require('./leaderboard'));
-
+router.use('/comments', require('./comments'));
 module.exports = router;
